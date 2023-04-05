236f1de5622ae0cb736620c7964013dba78f2273
Andrew Ross <andyross4@gmail.com>
2023-04-05 15:25:57 -0400

    initial commit

---
+++ ReactTemplate/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div>Content</div>
+  );
+}
+
+root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
