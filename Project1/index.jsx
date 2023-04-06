d09406db1fffc3c4d36e0b24ba0d57a84f25125a
Andrew Ross <andyross4@gmail.com>
2023-04-06 12:18:45 -0400

    feat: add tailwind to test project

--- Project1/index.jsx
+++ Project1/index.jsx
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 function App() {
   return (
-    <div>New Content</div>
+    <div>
+      <img src="/react-logo.png" width="40"/>
+      <h1 className="text-3xl font-bold mb-4">Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
   );
 }
 
