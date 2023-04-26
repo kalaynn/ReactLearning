696d1eca0548c176c2d0a826248aee0cd527a2f6
Andrew Ross <andyross4@gmail.com>
2023-04-26 12:33:17 -0400

    chore: refactoring

--- Project2/components/CardCarousel.tsx
+++ Project2/components/CardCarousel.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Card, CardProps } from './Card'
+import cardImageSwimmingLessons from '../images/card-image-swimming-lessons.png'
+import cardImageWeddingPhotography from '../images/card-image-wedding-photography.png'
+import cardImageMountainBiking from '../images/card-image-mountain-biking.png'
 
-
-export interface CardCarouselProps {
-    cards: CardProps[]
-}
-
-export function CardCarousel({cards}: CardCarouselProps) {
+export default function CardCarousel() {
     return (
         <div className="mt-6">
             <div className="flex flex-row overflow-auto">
-                {cards.map((card, index) => (
+                {Cards().map((card, index) => (
                     <Card
                         title={card.title}
                         reviewStars={card.reviewStars}
@@ -26,4 +24,39 @@ export function CardCarousel({cards}: CardCarouselProps) {
             </div>
         </div>
     );
+}
+
+function Cards(): CardProps[] {
+    return [
+        {
+            title: "Life lessons with Katie Zaferes",
+            reviewStars: "5.0",
+            reviewCount: 6,
+            location: "USA",
+            cost: 136,
+            imageSrc: cardImageSwimmingLessons,
+            isSoldOut: true,
+            isOnline: false
+        },
+        {
+            title: "Learn wedding photography",
+            reviewStars: "5.0",
+            reviewCount: 30,
+            location: "USA",
+            cost: 125,
+            imageSrc: cardImageWeddingPhotography,
+            isSoldOut: false,
+            isOnline: true
+        },
+        {
+            title: "Group Mountain Biking",
+            reviewStars: "4.8",
+            reviewCount: 2,
+            location: "USA",
+            cost: 50,
+            imageSrc: cardImageMountainBiking,
+            isSoldOut: false,
+            isOnline: false
+        }
+    ]
 }
--- Project2/components/Main.tsx
+++ Project2/components/Main.tsx
@@ -1,51 +1,12 @@
 import React from 'react';
 import Hero from './Hero'
-import { CardCarousel } from './CardCarousel'
-import { CardProps } from './Card'
-import cardImageSwimmingLessons from '../images/card-image-swimming-lessons.png'
-import cardImageWeddingPhotography from '../images/card-image-wedding-photography.png'
-import cardImageMountainBiking from '../images/card-image-mountain-biking.png'
-
-function Cards(): CardProps[] {
-    return [
-        {
-            title: "Life lessons with Katie Zaferes",
-            reviewStars: "5.0",
-            reviewCount: 6,
-            location: "USA",
-            cost: 136,
-            imageSrc: cardImageSwimmingLessons,
-            isSoldOut: true,
-            isOnline: false
-        },
-        {
-            title: "Learn wedding photography",
-            reviewStars: "5.0",
-            reviewCount: 30,
-            location: "USA",
-            cost: 125,
-            imageSrc: cardImageWeddingPhotography,
-            isSoldOut: false,
-            isOnline: true
-        },
-        {
-            title: "Group Mountain Biking",
-            reviewStars: "4.8",
-            reviewCount: 2,
-            location: "USA",
-            cost: 50,
-            imageSrc: cardImageMountainBiking,
-            isSoldOut: false,
-            isOnline: false
-        }
-    ]
-}
+import CardCarousel from './CardCarousel'
 
 export default function Main() {
     return (
         <div className="m-6">
             <Hero/>
-            <CardCarousel cards={Cards()}/>
+            <CardCarousel/>
         </div>
     );
 }
